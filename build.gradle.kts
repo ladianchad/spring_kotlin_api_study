c85dcd0d6f0e7c28032e0162654ae3077a0128ef
ladianchad <qhrejddlvltm@gmail.com>
2022-02-15 22:28:35 +0900

    swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	//추가
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
