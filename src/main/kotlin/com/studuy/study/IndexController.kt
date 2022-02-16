1c0f94e0530094e2a61baab6ce23676c87c0dae4
ladianchad <qhrejddlvltm@gmai.com>
2022-02-16 19:50:43 +0900

    main page redirect

--- src/main/kotlin/com/studuy/study/IndexController.kt
+++ src/main/kotlin/com/studuy/study/IndexController.kt
@@ -12,6 +12,6 @@ class IndexController {
         model["title"] = "Spring Kotlin APi"
         model["comment"] = "Spring Kotlin API를 공부하기 위한 사이트 입니다."
         model["entity"] = "Device"
-        return "home"
+        return "redirect:http://study-ui.lachani.com"
     }
 }
