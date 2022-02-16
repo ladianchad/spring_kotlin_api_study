ef94e69e05aaac9ac254ef4b72dca86cb467d738
ladianchad <qhrejddlvltm@gmai.com>
2022-02-16 21:01:23 +0900

    config essential?

---
+++ src/main/kotlin/com/studuy/study/config/GlobalConfig.kt
@@ -0,0 +1,23 @@
+package com.studuy.study.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class GlobalConfig : WebMvcConfigurer{
+    override
+    fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://localhost:3000")
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.POST.name,
+                HttpMethod.PUT.name,
+                HttpMethod.DELETE.name,
+                HttpMethod.PATCH.name
+            )
+    }
+}
+
