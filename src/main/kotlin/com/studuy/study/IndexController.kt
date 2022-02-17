291581d7ad37622908d96ff412cc018a2e3d10d7
ladianchad <qhrejddlvltm@gmai.com>
2022-02-17 11:37:24 +0900

    action region

--- src/main/kotlin/com/studuy/study/IndexController.kt
+++ src/main/kotlin/com/studuy/study/IndexController.kt
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping
 class IndexController {
     @GetMapping("/")
     fun home(model: Model): String {
+        model["title"] = "Spring Kotlin APi"
+        model["comment"] = "Spring Kotlin API를 공부하기 위한 사이트 입니다."
+        model["entity"] = "Device"
         return "home"
     }
 }
