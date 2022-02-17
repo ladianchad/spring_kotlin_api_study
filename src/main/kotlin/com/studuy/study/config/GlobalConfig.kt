70dac6d71973e32bac183213f9dccea850235472
ladianchad <qhrejddlvltm@gmai.com>
2022-02-17 13:52:29 +0900

    redirect

--- src/main/kotlin/com/studuy/study/config/GlobalConfig.kt
+++ src/main/kotlin/com/studuy/study/config/GlobalConfig.kt
@@ -1,8 +1,10 @@
 package com.studuy.study.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl
 import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
